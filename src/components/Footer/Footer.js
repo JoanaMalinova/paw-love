07422b1bed8540c1feb03a3551d9645c7f0f4c50
Joana Malinova <ani.mani.nen@gmail.com>
2023-08-30 18:52:36 +0300

    changed font

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import styles from "../../styles/Footer.module.css";
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer className={styles.footer}>
-        <p>&copy; PAW<span>Love</span> 2023 All rights reserved </p>
+            <p>&copy; <span>PAWLove</span> 2023 All rights reserved </p>
         </footer>
     )
 }
