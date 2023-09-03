c9c75f4823fd91789e71fc03c6d74fa52ac988c6
Joana Malinova <ani.mani.nen@gmail.com>
2023-09-04 00:12:17 +0300

    changed comment design in detaiils

--- src/components/Details/Comment.js
+++ src/components/Details/Comment.js
@@ -3,9 +3,12 @@ import styles from '../../styles/Details.module.css';
 export function Comment({ comment, username, created }) {
 
     return (
-        <li className={styles["comment-box"]}>
-            <p className={styles['main']}><span className={styles['username']}>{username}:</span> <span className={styles['comment']}>{comment}</span></p>
-            <p className={styles['sub']}>{created}</p>
+        <li className={styles['comment-list-item']}>
+            <span className={styles['username']}>{username}:</span>
+            <div className={styles["comment-box"]}>
+                <p className={styles['main']}><span className={styles['comment']}>{comment}</span></p>
+                <p className={styles['sub']}>{created}</p>
+            </div>
         </li>
     )
 }      
